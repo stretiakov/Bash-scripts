cda691b83f8f43a8bcef40fa67e8eff19d5f9cb5
Serhii Tretiakov <142805080+stretiakov@users.noreply.github.com>
2024-01-10 22:34:21 +0100

    Update shift_change.sh

--- shift_monitor/shift_change.sh
+++ shift_monitor/shift_change.sh
@@ -15,7 +15,7 @@ body="$(cat to_email.txt)"
 curr_xls="$(pwd)/$(tail -2 to_email.txt | head -1 | cut -d ':' -f2)"
 old_xls="$(pwd)/$(tail -1 to_email.txt | cut -d ':' -f2)"
 
-# emailing only if an event occurs: mismatch in array length or a shift change
+# emailing only if an event occurs: mismatch in array length, a shift change or a new schedule is added
 # appending the XLSX files before and after change
 if [[ "$subject" = "Shift change!" ]] || [[ "$subject" = "Data extraction error!" ]] || [[ "$subject" = "A new schedule was added!" ]]
 then
